63dcc976a115840f0a7393a554da4103094aa150
memamun <a.a.mamun595@gmail.com>
2026-09-24 02:14:45 +0600

    fix: preserve original status bar pre-rendered font bitmaps exactly as in v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,7 @@ android {
   buildTypes {
     release {
       isCrunchPngs = false
-      isMinifyEnabled = true
-      isShrinkResources = true
+      isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
